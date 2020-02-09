2ec0914c59e6ad5d865c8d82008d902410616558
LeadKiller <hl2epicfan@gmail.com>
2020-02-09 03:57:13 -0800

    add better fake ping

--- lua/leadbot/modules/ping/sh_ping.lua
+++ lua/leadbot/modules/ping/sh_ping.lua
@@ -1,9 +1,24 @@
 local meta = FindMetaTable("Player")
 local oldFunc = meta.Ping
-local ping = -1
+local ping = -0
+local fakeping = true
+local convar = CreateConVar("leadbot_fakeping", "0", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Enables fakeping (Do not use this in public servers, only LAN/Singleplayer. It can get you blacklisted and possibly banned!)")
 
 function meta:Ping()
-    if self:IsBot() then
+    if convar:GetBool() and self:IsBot() then
+        if fakeping then
+            self.FakePing = self.FakePing or math.random(35, 105)
+            self.OFakePing = self.OFakePing or self.FakePing
+
+            if math.random(125) == 1 then
+                self.FakePing = self.OFakePing + math.random(3)
+            elseif math.random(125) == 1 then
+                self.FakePing = self.OFakePing - math.random(3)
+            end
+
+            return self.FakePing
+        end
+
         return ping
     else
         return oldFunc(self)
