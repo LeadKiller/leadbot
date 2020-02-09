8c9ce89b736cc114ec5487bc51d03dbd68008388
LeadKiller <hl2epicfan@gmail.com>
2020-02-09 03:56:48 -0800

    afk system update

--- lua/leadbot/modules/afk/cl_afk.lua
+++ lua/leadbot/modules/afk/cl_afk.lua
@@ -1,21 +1,28 @@
 hook.Add("HUDPaint", "AFKT", function()
     if LocalPlayer():GetNWBool("LeadBot_AFK") then
         local aa = 200 + math.sin(CurTime() * 5) * 50
-        draw.SimpleTextOutlined("You are currently AFK", "ScoreboardDefaultTitle", ScrW() / 2, ScrH() / 1.7, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM, 1,  Color(0, 0, 0,255))
-        draw.SimpleTextOutlined("Jump to get out of AFK", "ScoreboardDefaultTitle", ScrW() / 2, ScrH() / 1.6, Color(255, 255, 255, aa), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM, 1, Color(0, 0, 0, aa))
+        draw.SimpleTextOutlined("You are currently AFK", "DermaLarge", ScrW() / 2, ScrH() / 1.7, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM, 1,  Color(0, 0, 0,255))
+        draw.SimpleTextOutlined("Jump to get out of AFK", "DermaLarge", ScrW() / 2, ScrH() / 1.6, Color(255, 255, 255, aa), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM, 1, Color(0, 0, 0, aa))
     end
 end)
 
-local tp = false
-local NA = Angle(0, 0, 0)
+-- local tp = false
+-- local NA = Angle(0, 0, 0)
+local lastsend = CurTime()
 
 hook.Add("CreateMove", "LeadBot_AFK", function(cmd)
-    if LocalPlayer():GetNWBool("LeadBot_AFK") and cmd:KeyDown(IN_JUMP) or cmd:KeyDown(IN_FORWARD) then
-        net.Start("LeadBot_AFK_Off")
-        net.SendToServer()
+    if LocalPlayer():GetNWBool("LeadBot_AFK") then
+        if lastsend < CurTime() and cmd:KeyDown(IN_JUMP) or cmd:KeyDown(IN_FORWARD) then
+            net.Start("LeadBot_AFK_Off")
+            net.SendToServer()
+            lastsend = CurTime() + 5
+        end
+
+        cmd:ClearButtons()
+        cmd:ClearMovement()
     end
 
-    if LocalPlayer():KeyReleased(IN_ATTACK) then
+    --[[if LocalPlayer():KeyReleased(IN_ATTACK) then
         -- tp = !tp
     end
 
@@ -23,10 +30,12 @@ hook.Add("CreateMove", "LeadBot_AFK", function(cmd)
         local s = GetConVar("m_pitch"):GetFloat()
         NA.pitch = math.Clamp(NA.pitch + cmd:GetMouseY() * s, -90, 90)
         NA.yaw = NA.yaw - cmd:GetMouseX() * s
-    end
+    end]]
 end)
 
-local ang
+-- looked pretty bad :(
+
+--[[local ang
 
 hook.Add("CalcView", "LeadBot_AFK", function(ply, origin, angles)
     if !ang then ang = angles end
@@ -60,4 +69,4 @@ end)
 hook.Add("CalcViewModelView", "LeadBot_AFK", function(wep, vm, oldpos, oldang, newpos, newang)
     if !LocalPlayer():GetNWBool("LeadBot_AFK") then return end
     return oldpos, ang
-end)
+end)]]
--- lua/leadbot/modules/afk/sv_afk.lua
+++ lua/leadbot/modules/afk/sv_afk.lua
@@ -1,6 +1,8 @@
 concommand.Add("leadbot_afk", function(ply, _, args) if ply:IsSuperAdmin() then LeadBot.Botize(ply) end end, nil, "Adds a LeadBot ;)")
 
 local time = CreateConVar("leadbot_afk_timetoafk", "300", {FCVAR_ARCHIVE})
+local meta = FindMetaTable("Player")
+local oldFunc = meta.IsBot
 
 util.AddNetworkString("LeadBot_AFK_Off")
 
@@ -10,6 +12,8 @@ net.Receive("LeadBot_AFK_Off", function(_, ply)
 end)
 
 hook.Add("PlayerTick", "LeadBot_AFK", function(ply)
+    if !time:GetBool() then return end
+
     ply.LastAFKCheck = ply.LastAFKCheck or CurTime() + time:GetFloat()
 
     if ply:KeyDown(IN_FORWARD) or ply:KeyDown(IN_BACK) or ply:KeyDown(IN_MOVELEFT) or ply:KeyDown(IN_MOVERIGHT) or ply:KeyDown(IN_ATTACK) then
@@ -24,6 +28,10 @@ end)
 
 function LeadBot.Botize(ply, togg)
     if togg == nil then togg = !ply.LeadBot end
+
+    if ((!togg and ply.LeadBot) or (togg and !ply.LeadBot)) and LeadBot.SuicideAFK and ply:Alive() then
+        ply:Kill()
+    end
 
     if !togg then
         ply:SetNWBool("LeadBot_AFK", false)
@@ -45,5 +53,16 @@ function LeadBot.Botize(ply, togg)
         ply.ControllerBot:SetOwner(ply)
         ply.LastSegmented = CurTime()
         ply.CurSegment = 2
+        if GetConVar("leadbot_strategy"):GetBool() then
+            ply.BotStrategy = math.random(0, 1)
+        end
+    end
+end
+
+function meta:IsBot()
+    if self.LeadBot and LeadBot.AFKBotOverride then
+        return true
+    else
+        return oldFunc(self)
     end
 end
