c4cea62f674532dad9aba7f770a72859c4d47c6f
LeadKiller <hl2epicfan@gmail.com>
2020-06-09 22:49:54 -0700

    zombie plague bots now use current goal system

--- lua/leadbot/gamemodes/zombieplague.lua
+++ lua/leadbot/gamemodes/zombieplague.lua
@@ -8,221 +8,129 @@ function LeadBot.AddBotOverride(bot)
     RoundManager:AddPlayerToPlay(bot)
 end
 
-local humancmd
-local zombiecmd
-local humanmove
-local zombiemove
-
-function LeadBot.StartCommand(bot, cmd)
-    local buttons = 0
-    local hbuttons = 0
-
-    if bot.NextJump == 0 then
-        bot.NextJump = CurTime() + 1
-        buttons = buttons + IN_JUMP
-    end
-
-    if !bot:IsOnGround() and bot.NextJump > CurTime() then
-        buttons = buttons + IN_DUCK
-    end
-
-    if bot:Team() == TEAM_HUMANS then
-        hbuttons = humancmd(bot, cmd)
-    else
-        hbuttons = zombiecmd(bot, cmd)
-    end
-
-    cmd:ClearButtons()
-    cmd:ClearMovement()
-    cmd:SetButtons(buttons + hbuttons)
-end
 
 function LeadBot.PlayerMove(bot, cmd, mv)
-    bot.Forget = bot.Forget or CurTime()
-    bot.NextJump = bot.NextJump or CurTime()
+    local controller = bot.ControllerBot
 
-    if !IsValid(bot.ControllerBot) then
+    if !IsValid(controller) then
         bot.ControllerBot = ents.Create("leadbot_navigator")
         bot.ControllerBot:Spawn()
         bot.ControllerBot:SetOwner(bot)
+        controller = bot.ControllerBot
     end
 
-    if bot.ControllerBot:GetPos() ~= bot:GetPos() then
-        bot.ControllerBot:SetPos(bot:GetPos())
+    -- force a recompute
+    if controller.PosGen and controller.P and controller.TPos ~= controller.PosGen then
+        controller.TPos = controller.PosGen
+        controller.P:Compute(controller, controller.PosGen)
     end
 
-    if bot:Team() == TEAM_HUMANS then
-        humanmove(bot, cmd, mv)
-    else
-        zombiemove(bot, cmd, mv)
-    end
-end
-
-function zombiecmd(bot, cmd)
-    local buttons = 0
-
-    if IsValid(bot.TargetEnt) and bot.SeeTarget then
-        local attack = IN_ATTACK
-        if math.random(2) == 1 then
-            attack = IN_ATTACK2
-        end
-        buttons = buttons + attack
+    if controller:GetPos() ~= bot:GetPos() then
+        controller:SetPos(bot:GetPos())
     end
 
-    return buttons
-end
-
-function humancmd(bot, cmd)
-    local buttons = IN_SPEED
-
-    if IsValid(botWeapon) and (botWeapon:Clip1() == 0 or !IsValid(bot.TargetEnt) and botWeapon:Clip1() <= botWeapon:GetMaxClip1() / 2) then
-        buttons = buttons + IN_RELOAD
-    end
-
-    if IsValid(bot.TargetEnt) and bot.SeeTarget and math.random(2) == 1 then
-        buttons = buttons + IN_ATTACK
-    end
-
-    return buttons
-end
-
-local function navigate(bot, cmd, mv, goal, segments)
     mv:SetForwardSpeed(1200)
-    bot.ControllerBot.PosGen = bot.botPos
-
-    if !bot.ControllerBot.P then
-        return
+    -- main thing that's keeping the bots from being lag free is seeking targets
+    -- losing about 4-25 fps with this
+    -- for now, using player.GetAll() rather than ents.GetAll()
+    -- having no npc support is bad, but I think most people will use this for dm
+    if (bot.NextSpawnTime and bot.NextSpawnTime + 1 > CurTime()) or !IsValid(controller.Target) or controller.ForgetTarget < CurTime() or controller.Target:Health() < 1 then
+        controller.Target = nil
     end
 
-    local segments = bot.ControllerBot.P:GetAllSegments()
+    if !IsValid(controller.Target) then
+        for _, ply in ipairs(player.GetAll()) do
+            if ply ~= bot and ((ply:IsPlayer() and (!LeadBot.TeamPlay or (LeadBot.TeamPlay and (ply:Team() ~= bot:Team())))) or ply:IsNPC()) and ply:GetPos():DistToSqr(bot:GetPos()) < 2250000 then
+                local targetpos = ply:EyePos() - Vector(0, 0, 10)
+                local trace = util.TraceLine({
+                    start = bot:GetShootPos(),
+                    endpos = targetpos,
+                    filter = function(ent) return ent == ply end
+                })
 
-    if !segments then return end
-
-    local curgoal = segments[2]
-
-    if !curgoal then return end
-
-    if segments[3] and segments[3].pos.z > bot:GetPos().z + 6 and bot.NextJump < CurTime() then
-        bot.NextJump = 0
-    end
-
-    local cur_segment = 1
-
-    -- think 15 steps ahead!
-    for i, segment in pairs(segments) do
-        if i < 5 then
-            if bot:VisibleVec(segment.pos) and i > cur_segment then
-                cur_segment = i
+                if trace.Entity == ply then
+                    controller.Target = ply
+                    controller.ForgetTarget = CurTime() + 2
+                end
             end
         end
     end
 
-    curgoal = segments[cur_segment]
-
-    if bot == Entity(1) then
-        bot.ControllerBot.P:Draw()
-    end
-
-    local lerp = 3
-    local lerpc = 2
-
-    if !LeadBot.LerpAim then
-        lerp = 100
-        lerpc = 100
-    end
-
-    local mva = ((curgoal.pos + Vector(0, 0, 65)) - bot:GetShootPos()):Angle()
-
-    if Vector(curgoal.pos.x, curgoal.pos.y, 0):DistToSqr(Vector(bot:GetPos().x, bot:GetPos().y, 0)) < 500 then
-        if segments[cur_segment + 1] then
-            curgoal = segments[cur_segment + 1]
-        else
-            mva = Angle(0, 0, 0)
-            mv:SetForwardSpeed(0)
-        end
-        
-        -- recalculating
-    end
-
-    if mva ~= Angle(0, 0, 0) then
-        mv:SetMoveAngles(mva)
-    else
-        mv:SetForwardSpeed(0)
-    end
-
-    if mv:GetForwardSpeed() == 0 then return end
-
-    if IsValid(bot.TargetEnt) and bot:GetEyeTrace().Entity ~= bot.TargetEnt then
-        local shouldvegoneforthehead = bot.TargetEnt:EyePos()
-        local group = math.random(0, bot.TargetEnt:GetHitBoxGroupCount() - 1)
-        local bone = bot.TargetEnt:GetHitBoxBone(math.random(0, bot.TargetEnt:GetHitBoxCount(group) - 1), group) or 0
-        shouldvegoneforthehead = bot.TargetEnt:GetBonePosition(bone)
-        bot:SetEyeAngles(LerpAngle(FrameTime() * lerp, bot:EyeAngles(), (shouldvegoneforthehead - bot:GetShootPos()):Angle())) --[[+ bot:GetViewPunchAngles()]]
-    elseif bot:GetPos():DistToSqr(curgoal.pos) > 400 then
-        bot:SetEyeAngles(LerpAngle(FrameTime() * lerpc, bot:EyeAngles(), mva))
-    end
-
-    local eyea = bot:EyeAngles()
-
-    bot:SetEyeAngles(Angle(eyea.p, eyea.y, 0))
-end
-
-local function doorai(bot)
     local dt = util.QuickTrace(bot:EyePos(), bot:GetForward() * 45, bot)
 
     if IsValid(dt.Entity) and dt.Entity:GetClass() == "prop_door_rotating" then
         dt.Entity:Fire("Open","",0)
     end
-end
 
-local function targetai(bot)
-    bot.SeeTarget = false
-    if IsValid(bot.TargetEnt) then
-        bot.SeeTarget = util.TraceLine({start = bot:GetShootPos(), endpos = bot.TargetEnt:EyePos() - Vector(0, 0, 10), filter = function(ent) return ent == bot.TargetEnt end}).Entity == bot.TargetEnt
-    end
+    if !IsValid(controller.Target) and (!controller.PosGen or bot:GetPos():DistToSqr(controller.PosGen) < 1000 or controller.LastSegmented < CurTime()) then
+        -- find a random spot on the map, and in 10 seconds do it again!
+        controller.PosGen = controller:FindSpot("random", {radius = 12500})
+        controller.LastSegmented = CurTime() + 10
+    elseif IsValid(controller.Target) then
+        -- move to our target
+        local distance = controller.Target:GetPos():DistToSqr(bot:GetPos())
+        controller.PosGen = controller.Target:GetPos()
 
-    if !IsValid(bot.TargetEnt) then
-        for _, ply in pairs(player.GetAll()) do
-            if ply ~= bot --[[and ply:GetPos():DistToSqr(bot:GetPos()) < 2250000]] and ply:Team() ~= bot:Team() then
-                local targetpos = ply:EyePos() - Vector(0, 0, 10)
-                local trace = util.TraceLine({start = bot:GetShootPos(), endpos = targetpos, filter = function(ent) return ent == ply end})
-
-                if trace.Entity == ply then
-                    bot.TargetEnt = ply
-                end
-            end
-        end
-    elseif !bot.TargetEnt:Alive() or bot.TargetEnt:Team() == bot:Team() or bot.Forget < CurTime() and !bot.SeeTarget then
-        bot.TargetEnt = nil
-    elseif bot.Forget < CurTime() then
-        bot.Forget = CurTime() + 3
-    end
-end
-
-local function movementai(bot, cmd, mv)
-    if !IsValid(bot.TargetEnt) and (!bot.botPos or bot:GetPos():DistToSqr(bot.botPos) < 25000 --[[3600]] or math.abs(bot.LastSegmented - CurTime()) > 10) then
-        bot.botPos = bot.ControllerBot:FindSpot("random", {radius = 22500})
-        bot.LastSegmented = CurTime()
-    elseif IsValid(bot.TargetEnt) then
-        local distance = bot.TargetEnt:GetPos():DistToSqr(bot:GetPos())
-        bot.botPos = bot.TargetEnt:GetPos()
-
+        -- back up if the target is really close
+        -- TODO: find a random spot rather than trying to back up into what could just be a wall
+        -- something like controller.PosGen = controller:FindSpot("random", {pos = bot:GetPos() - bot:GetForward() * 350, radius = 1000})?
         if (bot:Team() == TEAM_ZOMBIES and distance <= 2000) or distance <= 160000 then
             mv:SetForwardSpeed(-1200)
         end
     end
-end
 
-function zombiemove(bot, cmd, mv)
-    targetai(bot)
-    doorai(bot)
-    movementai(bot, cmd, mv)
-    navigate(bot, cmd, mv)
-end
+    -- movement also has a similar issue, but it's more severe...
+    if !controller.P then
+        return
+    end
 
-function humanmove(bot, cmd, mv)
-    targetai(bot)
-    doorai(bot)
-    movementai(bot, cmd, mv)
-    navigate(bot, cmd, mv)
+    local segments = controller.P:GetAllSegments()
+
+    if !segments then return end
+
+    local cur_segment = controller.cur_segment
+    local curgoal = segments[cur_segment]
+
+    -- got nowhere to go, why keep moving?
+    if !curgoal then
+        mv:SetForwardSpeed(0)
+        return
+    end
+
+    -- think every step of the way!
+    -- TODO: corner turning like nextbot npcs
+    if segments[cur_segment + 1] and Vector(bot:GetPos().x, bot:GetPos().y, 0):DistToSqr(Vector(curgoal.pos.x, curgoal.pos.y)) < 100 then
+        controller.cur_segment = controller.cur_segment + 1
+        curgoal = segments[controller.cur_segment]
+    end
+
+    -- jump
+    if controller.NextJump ~= 0 and curgoal.pos.z > (bot:GetPos().z + 16) and controller.NextJump < CurTime() then
+        controller.NextJump = 0
+    end
+
+    if GetConVar("developer"):GetBool() then
+        controller.P:Draw()
+    end
+
+    -- eyesight
+    local lerp = FrameTime() * math.random(8, 10)
+    local lerpc = FrameTime() * 8
+
+    if !LeadBot.LerpAim then
+        lerp = 1
+        lerpc = 1
+    end
+
+    local mva = ((curgoal.pos + bot:GetViewOffset()) - bot:GetShootPos()):Angle()
+
+    mv:SetMoveAngles(mva)
+
+    if IsValid(controller.Target) then
+        bot:SetEyeAngles(LerpAngle(lerp, bot:EyeAngles(), (controller.Target:EyePos() - bot:GetShootPos()):Angle()))
+        return
+    else
+        local ang = LerpAngle(lerpc, bot:EyeAngles(), mva)
+        bot:SetEyeAngles(Angle(ang.p, ang.y, 0))
+    end
 end
